62da5b32e46947a50c9534e56c1edbf1ea2123c6
godwinj96 <godwinjames670@gmail.com>
2024-07-08 23:06:07 +0100

    updated static config

--- iwemibackendapi/settings.py
+++ iwemibackendapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kmc-v*8*@=enugql&cq0!bqu)#9mcz9z_sf#v+wx)d7edof$dc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
